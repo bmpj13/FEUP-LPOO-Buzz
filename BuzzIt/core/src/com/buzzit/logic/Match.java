05e3e6b649260d19a68b695a58185075d78b0886
João Barbosa <bmpj13@hotmail.com>
2016-06-04 14:30:02 +0100

    test dependency

--- BuzzIt/core/src/com/buzzit/logic/Match.java
+++ BuzzIt/core/src/com/buzzit/logic/Match.java
@@ -9,8 +9,8 @@ public class Match {
     private int questionIndex;
     private Player player;
 
-    public Match(int rounds){
-        this.questions = Play.getInstance().play(rounds);
+    public Match(int rounds, ArrayList<Category> categoriesChosen, Difficulty difficulty){
+        this.questions = Play.getInstance().play(rounds,categoriesChosen, difficulty);
         this.questionIndex = 0;
         player = new Player("UEUEUEUEUEUEUEUEUEUEUEUEUEUE");
     }
