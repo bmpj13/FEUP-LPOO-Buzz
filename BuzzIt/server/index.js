2124c47f2021a073bb1e35241e22f0b60f4ccdb3
ruimoliveira <ruimiguelalmeidaoliveira@gmail.com>
2016-06-05 19:50:38 +0100

    Add files via upload

---
+++ BuzzIt/server/index.js
@@ -0,0 +1,66 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var clients = 0;
+var players = [];
+var question;
+var answerOptions;
+
+server.listen(8080,function(){
+	console.log("Server is now running...")
+});
+
+io.on('connection' , function(socket){
+    clients = clients+1;
+
+    if(clients == 1){
+        server.close();
+    }
+
+	console.log("Player Connected!");
+
+	socket.emit('socketID',{ id: socket.id })
+	socket.emit('getPlayers', { id: socket.id })
+	socket.emit('isPlayer1', { n: clients });
+
+	socket.broadcast.emit('newPlayer', { id: socket.id });
+
+    if(clients == 1){
+        server.listen(8080);
+    }
+
+    socket.on('playerIsReady', function(data){
+        data.id = socket.id;
+
+    });
+
+	socket.on('disconnect', function(){
+        clients = clients-1;
+		console.log("Player Disconnected");
+		socket.broadcast.emit('playerDisconnected', { id: socket.id });
+		for(var i=0; i<players.length; i++){
+		    if(players[i].id == socket.id){
+		        players.splice(i, 1);
+		    }
+		}
+	});
+
+	players.push(new player("playerName"), socket.id);
+});
+
+function player(name, id){
+    this.id = id;
+    this.name = name;
+    this.points = 0;
+}
+
+function question(questionString){
+    this.questionString = questionString;
+}
+
+function answerOptions(optionA, optionB, optionC, optionD){
+    this.optionA = optionA;
+    this.optionB = optionB;
+    this.optionC = optionC;
+    this.optionD = optionD;
+}
