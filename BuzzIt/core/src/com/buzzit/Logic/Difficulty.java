44437abb1b02e0bbc45d1d40d5326d22a3c9dd32
William <wnfukunaga@gmail.com>
2016-06-05 23:42:58 +0100

    allows for different difficulties

--- BuzzIt/core/src/com/buzzit/Logic/Difficulty.java
+++ BuzzIt/core/src/com/buzzit/Logic/Difficulty.java
@@ -2,17 +2,23 @@ package com.buzzit.Logic;
 
 
 public enum Difficulty {
-    EASY(5),
-    MEDIUM(10),
-    HARD(20);
+    EASY(5, 0),
+    MEDIUM(10, 1),
+    HARD(20, 2);
 
     private int points;
+    private int index;
 
-    Difficulty(int points){
+    Difficulty(int points, int index){
         this.points = points;
+        this.index = index;
     }
 
     public int getPoints() {
         return points;
+    }
+
+    public int getIndex(){
+        return index;
     }
 }
--- BuzzIt/core/src/com/buzzit/Logic/Play.java
+++ BuzzIt/core/src/com/buzzit/Logic/Play.java
@@ -14,8 +14,14 @@ import java.util.Random;
 public class Play {
 
 
-	static private ArrayList<ArrayList<Question>> questionsByCategory;
-	
+	//static private ArrayList<ArrayList<Question>> questionsByCategory;
+	static private ArrayList<Question>[][] questions;
+
+	private final String easyPath = "data/easy.properties";
+	private final String mediumPath = "data/medium.properties";
+	private final String hardPath = "data/hard.properties";
+
+	private Thread easyThread, mediumThread, hardThread;
 
 	private static Play ourInstance = new Play();
 
@@ -24,53 +30,84 @@ public class Play {
 	}
 
 	private Play() {
-
-		questionsByCategory = new ArrayList<ArrayList<Question>>();
+		questions = new ArrayList[Difficulty.values().length][Category.values().length];
 		for(Category cat:Category.values()){
-			ArrayList<Question> questions = new ArrayList<Question>();
-			questionsByCategory.add(questions);
+			ArrayList<Question> q = new ArrayList<>();
+			questions[Difficulty.EASY.getIndex()][cat.getIndex()] = q;
+			q = new ArrayList<>();
+			questions[Difficulty.MEDIUM.getIndex()][cat.getIndex()] = q;
+			q = new ArrayList<>();
+			questions[Difficulty.HARD.getIndex()][cat.getIndex()] = q;
+
 		}
 
-		getFile(Difficulty.MEDIUM);
+//		easyThread = new Thread(){
+//			public void run(){
+//				getFile(Difficulty.EASY, Play.questions[Difficulty.EASY.getIndex()], easyPath);
+//			}
+//		};
+//		mediumThread = new Thread(){
+//			public void run(){
+//				getFile(Difficulty.MEDIUM, Play.questions[Difficulty.MEDIUM.getIndex()], mediumPath);
+//			}
+//		};
+//		hardThread = new Thread(){
+//			public void run(){
+//				getFile(Difficulty.HARD, Play.questions[Difficulty.HARD.getIndex()], hardPath);
+//			}
+//		};
+//		easyThread.start();
+//		mediumThread.start();
+//		hardThread.start();
+//
+//		try {
+//				easyThread.join(500);
+//				mediumThread.join(500);
+//				hardThread.join(500);
+//		} catch(InterruptedException ex){
+//			ex.printStackTrace();
+//			Gdx.app.log("EXCEPTION","Interrupted Exception");
+//		}
+		getFile(Difficulty.EASY, questions[Difficulty.EASY.getIndex()], easyPath);
+		getFile(Difficulty.MEDIUM, questions[Difficulty.MEDIUM.getIndex()], mediumPath);
+		getFile(Difficulty.HARD, questions[Difficulty.HARD.getIndex()], hardPath);
 	}
 
 
-	public void getFile(Difficulty difficulty){
+	public void getFile(Difficulty difficulty, ArrayList<Question>[] questionsByCategory, String filepath){
 		Properties prop = new Properties();
 		InputStream input = null;
 
 		try {
-			String filepath = "assets/data/a.properties";
-			input = getClass().getClassLoader().getResourceAsStream(filepath);
+			FileHandle file = Gdx.files.internal(filepath);
+			input = file.read();
 
 			if (input == null) throw new IllegalArgumentException(filepath);
 
 			//load a properties file from class path, inside static method
 			prop.load(input);
 
-			//get the property value and print it out
+			//get the property value and store it
 			int t = Integer.parseInt(prop.getProperty("numquestions"));
 			for(int i = 0; i < t; i++){
 				String question = prop.getProperty("question"+i);
 				int answers = Integer.parseInt(prop.getProperty("numanswers" + i));
 				String category = prop.getProperty("category" + i);
 				Category cat = Category.getCategory(category);
-				int arrayIndex = cat.getIndex();
+				int categoryIndex = cat.getIndex();
 
 				String correct = new String();
 				ArrayList<String> wrong = new ArrayList<String>();
-				for(int z =0; z < answers; z++){
-					String key = prop.getProperty("answer"+i+"."+z);
-					if(z == 0){
+				for(int z =0; z < answers; z++) {
+					String key = prop.getProperty("answer" + i + "." + z);
+					if (z == 0) {
 						correct = key;
-					}
-					else{
+					} else {
 						wrong.add(key);
 					}
 				}
-
 				Question q = new Question(question, wrong, correct, difficulty, cat);
-				questionsByCategory.get(arrayIndex).add(q);
+				questionsByCategory[categoryIndex].add(q);
 			}
 
 		} catch (IOException ex) {
@@ -106,21 +143,20 @@ public class Play {
 		return indices;
 	}
 
-	private ArrayList<Question> getQuestionsFromCategory(ArrayList<Category> categoriesChosen){
+	private ArrayList<Question> getQuestionsFromCategory(ArrayList<Category> categoriesChosen, Difficulty difficulty){
 
 		ArrayList<Question> q = new ArrayList<Question>();
 		for(Category cat:categoriesChosen){
-			int index = cat.getIndex();
-			q.addAll(questionsByCategory.get(index));
+			q.addAll(Play.questions[difficulty.getIndex()][cat.getIndex()]);
 		}
 		return q;
 	}
 
 	public ArrayList<Question> play(int totalQuestions, ArrayList<Category> categoriesChosen, Difficulty difficulty){
-		ArrayList<Question> q = new ArrayList<Question>();
+		ArrayList<Question> q = new ArrayList<>();
 
 		// gets all questions from selected categories
-		ArrayList<Question> allFromCategories = getQuestionsFromCategory(categoriesChosen);
+		ArrayList<Question> allFromCategories = getQuestionsFromCategory(categoriesChosen, difficulty);
 
 		// get randomized question indices from each category
 		ArrayList<Integer> questionsScrambled = scramble(allFromCategories.size());
